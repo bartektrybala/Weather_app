42c9311fa6f69ab9a13c50662c96b578d1e03cf9
Bartek <bartektrybalaa@gmail.com>
2020-10-02 01:23:43 +0200

    User can add Cities by site

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,38 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
     api_key = '69238c44a6a9aee21feb1688f347b607'
-    city = 'London'
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    r = requests.get(url.format(city, api_key)).json()
+    form = CityForm()
 
-    city_weather = {
-        'city': r['name'],
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+    cities = City.objects.all()
 
-    context = {'city_weather': city_weather}
+    weather_data = []
+
+    for city in cities:
+
+
+        r = requests.get(url.format(city, api_key)).json()
+
+        city_weather = {
+            'city': r['name'],
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
 
